690add2dbc5da350593ea71bc5185dc305801311
qron <q.ferron@gmail.com>
2021-11-08 20:35:45 +0100

    feature: add room QRCode generation API

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
-const { fetchAllRooms, fetchFreeRooms, updateRoomStatus } = require('./controllers/rooms');
 const cors = require('cors');
 
-const app = express()
+const {
+	fetchAllRooms,
+	fetchFreeRooms,
+	updateRoomStatus,
+} = require('./controllers/rooms');
+
+const { generateQRCodeForRoom } = require('./controllers/qr-codes');
+
+const app = express();
+
 app.use(cors());
-const port = 3001
 
 app.get('/fetch-all-rooms', fetchAllRooms);
 app.get('/fetch-free-rooms', fetchFreeRooms);
 app.put('/update-room-status/:roomIndex/:userName', updateRoomStatus);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.get('/qr-code/:roomIndex.png', generateQRCodeForRoom);
+
+app.listen(3001, () => {
+	console.log('App listening on port 3001');
+});
